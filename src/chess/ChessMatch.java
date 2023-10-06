55f3b57ea974769e16a27b64ed0b407d3533cc52
Pedro Andrade <pedrogamerp@gmail.com>
2023-10-06 10:06:03 -0300

    feat: chess exception

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,8 +23,11 @@ public class ChessMatch {
         return aux;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(7, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        placeNewPiece('a',2, new King(board, Color.WHITE));
+        placeNewPiece('e',8, new King(board, Color.BLACK));
     }
 }
